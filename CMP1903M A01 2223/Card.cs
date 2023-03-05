64562646343f6fb900004b2f6830becdc0e1a810
miamikyu <26570204@students.lincoln.ac.uk>
2023-03-05 18:54:06 +0000

    Card Making

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -9,10 +9,46 @@ namespace CMP1903M_A01_2223
     class Card
     {
         //Base for the Card class.
+        //sets the private fields and public properties of the class
+
+        private int cardValue
+        public int Value 
+        private int suit 
+        public int Suit
+
         //Value: numbers 1 - 13
         //Suit: numbers 1 - 4
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+        public int Value {
+            get{
+                return Value;
+            } 
+            set{
+                if(value<15){
+                    Value=value;
+                }else{
+                    //Error
+                }
+            } 
+        }
+        //using the getter and setter for validation and to change numerical value to a string
+        public int Suit {
+            get{
+                return Suit;
+            } 
+            set{
+                if(value==1){
+                    Suit="Hearts";
+                }else if(value==2){
+                    Suit="Diamonds";
+                }else if(value==3){
+                    Suit="Clubs";
+                }else if(value==4){
+                    Suit="Spades";
+                }else{
+                    //Error
+                }
+            } 
+        }
     }
 }
