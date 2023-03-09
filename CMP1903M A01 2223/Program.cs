000fed9b3c97b969edb5c6453a1b92e7e0543ccb
miamikyu <26570204@students.lincoln.ac.uk>
2023-03-09 02:39:02 +0000

    up to date

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -22,7 +22,7 @@ namespace CMP1903M_A01_2223
                 Console.WriteLine("Enter 1 for Fisher Yates Shuffle");
                 Console.WriteLine("Enter 2 for Riffle Shuffle");
                 Console.WriteLine("Enter 3 for other options");
-            
+
 
                 string answer = Console.ReadLine();
                 if (answer == "1")
@@ -40,7 +40,7 @@ namespace CMP1903M_A01_2223
                     Console.WriteLine("For other options:");
                     Console.WriteLine("Enter 1 to deal 1 card");
                     Console.WriteLine("Enter 2 to deal an amount of cards of your choosing");
-    
+
                     string nextAnswer = Console.ReadLine();
                     if (nextAnswer == "1")
                     {
@@ -51,7 +51,7 @@ namespace CMP1903M_A01_2223
                     else if (nextAnswer == "2")
                     {
                         Console.WriteLine("Enter the amount of cards you would like to deal");
-                       
+
                         //takes the input as an int
                         int numOfCardsDealt = Int32.Parse(Console.ReadLine());
                         Pack.dealCard(numOfCardsDealt);
@@ -63,14 +63,7 @@ namespace CMP1903M_A01_2223
                         userContinue = true;
                     }
                 }
-            }
-
-                
-               
-                   
-            
-                
-            
+            }         
         }
     }
 }
